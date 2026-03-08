84164a6ace9bdd305ec48f00af2cf45a25864fff
Fryy_55 <ivan_ugu55@icloud.com>
2026-03-08 17:45:01 +0500

    Update the template to not set the icon manually on macOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QtSystemDetection>
 
 
 int main(int argc, char** argv) {
 	QApplication a(argc, argv);
-	a.setWindowIcon(QIcon(":/icon.png"));
-	QMainWindow w{};
+	#ifndef Q_OS_MACOS
+		a.setWindowIcon(QIcon(":/icon.png"));
+	#endif
 
+	QMainWindow w{};
 	w.show();
 	return a.exec();
 }
